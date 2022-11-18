ab15faf684fd75cc2cd9d5f3f0dfb8c90f5a4c1b
Rostislav Rabiec <rosta.rabiec@icloud.com>
2022-11-18 18:16:30 +0100

    Druhý commit

--- card.py
+++ card.py
@@ -11,29 +11,28 @@ def input_card_number():
 def reverse_card_number():
     card_number = input_card_number()
     reversed_card_number = card_number[::-1]
+    # kontrola print
+    print(reversed_card_number)
     return reversed_card_number
 
 
 # Spočítej sumu všech čísel na lichých indexech obráceného čísla karty
-def count_sum_of_odd_index():
+# Vyber čísla na sudých indexech, vynásob je dvěma, sečti jednotlivé cifry 
+# (dvojciferné rozděl na dvě čísla a sečti)
+def count_sum_of_odd_index ():
     reversed_card_number = reverse_card_number()
+
     odd_index = reversed_card_number[::2]
+    even_index = reversed_card_number[1::2]
+
     summary = 0
     for num in odd_index:
         summary += int(num)
-    return summary 
-
-
-# Vyber čísla na sudých indexech, vynásob je dvěma, sečti jednotlivé cifry (dvojciferné rozděl na dvě čísla a sečti)
-def count_even_index():
-    reversed_card_number = reverse_card_number()
-    even_index = reversed_card_number[1::2]
+    
     numbers = []
     for num in even_index:
         result = int(num) * 2
         numbers.append(result)
-    return numbers    
-
 
 
 # Sečti sumy lichých a upravených sudých indexů
@@ -42,4 +41,3 @@ def count_even_index():
 # Výslednou hodnotu vydělíš deseti a pokud bude zbytek po dělení nula, je číslo karty platné
 
 
-print(count_even_index())
